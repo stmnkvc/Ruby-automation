d968665b55644d6d66861c35307d94d6f1e4775a
Saša Stamenković <Saa Stamenkovi>
2017-08-23 00:13:48 +0200

    Ruby_second excercise

---
+++ Ruby_second.rb
@@ -0,0 +1,13 @@
+#acquireing customers name
+puts "Please enter your name:"
+#entaring variable name
+name = gets
+
+#acquireing customers surname
+puts "Please enter your surname"
+#entering varable surname
+surname = gets
+
+#promting hello message
+print "Hello Mr."+surname +name
+
